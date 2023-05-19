a93078e640293637f06cef49e1db6399fbcd2a01
Lauri Maila <laurikmaila@gmail.com>
2023-05-20 01:20:19 +0300

    11.10 done

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var serveStatic = require('serve-static')
 const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 app.use(serveStatic('dist', { index: ['default.html'] }))
