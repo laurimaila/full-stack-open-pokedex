90e418110345468dbf3b59427c3b044d82e19e4e
Lauri Maila <laurikmaila@gmail.com>
2023-05-20 02:54:25 +0300

    Added http_checks, changed cypress e2e address

--- cypress/e2e/pokepage.cy.js
+++ cypress/e2e/pokepage.cy.js
@@ -1,6 +1,6 @@
 describe('Pokedex', function () {
   it('pokemon page can be navigated to', function () {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:3000')
     cy.contains('raichu').click()
     cy.contains('static')
     cy.contains('raichu')
