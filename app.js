d33538f9c29223bbcd428d807ee6ad1409846669
Lauri Maila <laurikmaila@gmail.com>
2023-05-20 01:37:27 +0300

    Added health check to deployment, 11.11 done

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'))
-app.use(serveStatic('dist', { index: ['default.html'] }))
+app.use(serveStatic('dist', { index: ['index.html'] }))
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
