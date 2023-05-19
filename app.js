d0c4d01cc1d8486c5258164a3c20f06f5902bee6
Lauri Maila <laurikmaila@gmail.com>
2023-05-20 01:01:51 +0300

    Added fly.io deployment on push

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
+var serveStatic = require('serve-static')
 const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 
 app.use(express.static('dist'))
+app.use(serveStatic('dist', { index: ['default.html'] }))
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
