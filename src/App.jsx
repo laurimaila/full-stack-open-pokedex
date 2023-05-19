3afafce1bd6ca8e0759c2fb2afcebbe53313371d
Lauri Maila <laurikmaila@gmail.com>
2023-05-20 01:11:39 +0300

    Fixed workflow

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const mapResults = (({ results }) => results.map(({ url, name }) => ({
 })))
 
 const App = () => {
-  const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=63', mapResults)
+  const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=64', mapResults)
   if (isLoading) {
     return <LoadingSpinner />
   }
