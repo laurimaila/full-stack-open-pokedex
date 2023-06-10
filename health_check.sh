7232b6747601a262051641aca9ec1e10424852fb
Lauri Maila <laurikmaila@gmail.com>
2023-06-10 22:18:46 +0300

    Removed deprecated [[services.script_checks]

--- health_check.sh
+++ health_check.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+# Custom health checks like this don't work in fly.io right now...
 res=$(curl -s https://pokedex-lauri.fly.dev/health)
 
 if [ "$res" == "ok" ]; then
-  echo "Succeeded curl to /health but failed on purpose"
-  exit 1
+  echo "Succeeded curl to /health"
+  exit 0
 fi
 
 echo "Failed curl to /health"
